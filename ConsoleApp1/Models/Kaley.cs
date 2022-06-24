d4a8647f1df4e2943896647b43360de4f6362334
Kaley Moser <Kaley.Moser@fsbank.com>
2022-06-24 13:34:54 -0500

    Test

--- ConsoleApp1/Models/Kaley.cs
+++ ConsoleApp1/Models/Kaley.cs
@@ -1,8 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-public class Class1
+namespace ConsoleApp1.Models
 {
-	public Class1()
-	{
-	}
+    internal class Kaley
+    {
+    }
 }
